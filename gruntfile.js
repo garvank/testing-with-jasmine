a92721e7464ba8c6522f34768fd0aef2c3638869
Garvan Kuskey <gkuskey@invoca.com>
2016-10-26 16:03:49 -0700

    update gruntfile

---
+++ gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+  var uglifyBuildFiles = {},
+      package = grunt.file.readJSON('package.json');
+
+  // Configure Grunt
+  grunt.initConfig({
+    pkg: package,
+    jasmine: {
+      test: {
+        src: ["src/built/solutions.built.js", "test/helpers/stubs/*.js"],
+        options: {
+          keepRunner: true,
+          outfile: ".grunt/_SpecRunner.html",
+          vendor: ["test/helpers/*.js"],
+          specs: ["test/*_test.js"]
+        }
+      }
+    }
+  });
+
+  // Load plugins
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+  // Register tasks
+  grunt.registerTask('test', ['jasmine:test']);
+  grunt.registerTask('default', ['test']);
+};
