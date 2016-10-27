f991cc3279691daa2509f6613043bf55244e2df4
Garvan Kuskey <gkuskey@invoca.com>
2016-10-27 16:40:41 -0700

    fixed code

--- src/query_hash.js
+++ src/query_hash.js
@@ -1,13 +1,15 @@
 function queryHash(location) {
-  location    = location || window.location.href;
-  var hash    = {};
-  var queries = location.split('?')[1];
-  var pairs   = queries.split('&');
-    
-  pairs.forEach(function(pair) {
-    var params = pair.split('=');
-    hash[params[0]] = params[1];
-  }); 
+  location = location || window.location.href;
+  var queries, hash = {};
+
+  if (queries = location.split('?')[1]) {
+    var pairs   = queries.split('&');
+      
+    pairs.forEach(function(pair) {
+      var params = pair.split('=');
+      hash[params[0]] = params[1];
+    }); 
+  }
 
   return hash;
 }
